c98b9544f4a2fff203b9d6d035d7b8203d94ea2c
Natalya Myunster <natalyamyunster@yahoo.com>
2022-10-25 22:40:21 +0200

    added POST/transaction endpoint

--- dev/api.js
+++ dev/api.js
@@ -13,9 +13,8 @@ app.get('/blockchain', function (req, res) {
 });
 
 app.post('/transaction', function (req, res) {
-  console.log(req.body);
-  //res.send(`Amount of this transaction is ??? bitcoins`);
-  res.send(`Amount of this transaction is ${req.body.amount} bitcoins`);
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({note: `Transaction will be added in block ${blockIndex}.`})
 });
 
 app.get('/mine', function (req, res) {
